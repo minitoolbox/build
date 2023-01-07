2f213bbbe7e66ee336f1bb82d4fd9ae4ee084a5c
minitoolbox <121608442+minitoolbox@users.noreply.github.com>
2023-01-07 20:08:34 +0800

    update dates

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 (function ($) {
-    $.getUrlParam = function (name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) return decodeURI(r[2]);
-        return null;
-    };
+	$.getUrlParam = function (name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return decodeURI(r[2]);
+		return null;
+	};
 })(jQuery);
 //alert($.getUrlParam('page'))
 
 $.getJSON("data/cases.json", function (data) {
-    cases = data;
+	cases = data;
 
-    for (i in cases) {
-        $("#case-list").after(`
+	for (i in cases) {
+		if (cases[i].qq != 0) {
+			$("#case-list").after(`
             <div class="case">
 				<a href="${cases[i].url}">
 					<img class="case-img" src="${cases[i].mdimg}">
@@ -25,6 +26,25 @@ $.getJSON("data/cases.json", function (data) {
 						<div class="author-uid">${cases[i].authorUid}</div>
 					</div>
 				</div>
-			</div>`);
-    }
+			</div>`
+			);
+		}
+		else {
+			$("#case-list").after(`
+            <div class="case">
+				<a href="${cases[i].url}">
+					<img class="case-img" src="${cases[i].mdimg}">
+				</a>
+				<div class="case-name">${cases[i].name}</div>
+				<div class="author">
+					<img class="author-head" src="images/head.png">
+					<div class="author-info">
+						<div class="author-name">${cases[i].authorName}</div>
+						<div class="author-uid">${cases[i].authorUid}</div>
+					</div>
+				</div>
+			</div>`
+			);
+		}
+	}
 });
